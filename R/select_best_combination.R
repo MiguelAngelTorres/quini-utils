d10b14f8f97589bd0d1e5c2afe4c5706e7764dce
Miguel <torresma.1996@gmail.com>
2022-09-15 17:52:29 +0200

    Documentation of select_best_combination.R script

--- R/select_best_combination.R
+++ R/select_best_combination.R
@@ -1,6 +1,24 @@
 
 library(stats)
 
+#' Function to check if a sign has some distance conditions
+#'
+#' The function returns true if the new sign has the next two conditions:
+#' - At least a distance of min_diff with every sign in selected.
+#' - Joining with selected, at least there are total_diff matches with not all signs equal.
+#'
+#' @param data (data.table): A table with expected column 'sign'
+#' @param selected (list of integer): A list with the position in data of selected bets
+#' @param new (integer): the position in data of the new bet
+#' @param min_diff (integer): the min distance to check, sign by sign, between selected and new.
+#' @param total_diff (integer): the min matches with different results when joined seleted and new.
+#'
+#' @return is_diff (boolean): Wheter if new has the two conditions or not
+#'
+#' @export
+#' @examples
+#' is_diff_sign(data, selected, new, min_diff, total_diff)
+#'
 is_diff_sign <- function(data, selected, new, min_diff, total_diff){
   
   diffs = rep(FALSE, 14)
@@ -22,6 +40,25 @@ is_diff_sign <- function(data, selected, new, min_diff, total_diff){
 }
 
 
+#' Function that selects a set of bets splitting the selection
+#'
+#' Normally, if you select the bets by probability, rentability or em,
+#' consecutive bets have just 1 different sign. In order to avoid taking
+#' n very similar bets. This function provides a method to take splited bets
+#' with different signs in different matches.
+#'
+#' @param data (data.table): A table with expected column sign ordered by
+#' priority of taking (typically em, probability or rentability)
+#' @param nsigns (integer): number of bets you want to select
+#' @param diff (integer): min number of signs different between every 2
+#' selected bets.
+#'
+#' @return selected (data.table): Final bets selected
+#'
+#' @export
+#' @examples
+#' best_n_with_diff(data, nsigns, diff)
+#'
 best_n_with_diff <- function(data, nsigns, diff){
   
   
